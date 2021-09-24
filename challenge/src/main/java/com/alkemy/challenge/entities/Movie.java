8f1762cfb2be73de5bcff4323c626a97e1e07f65
USUARIO-PC\USUARIO <oscarruina@hotmail.com>
2021-09-24 11:00:36 -0300

    Documention

--- challenge/src/main/java/com/alkemy/challenge/entities/Movie.java
+++ challenge/src/main/java/com/alkemy/challenge/entities/Movie.java
@@ -13,7 +13,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
